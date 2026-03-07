190f0c95c3393858b544cc8f3a257511a007353c
Firoz-zaman <firozzaman.work@gmail.com>
2026-03-07 16:33:08 +0530

    feat: booking api , fix: treating params as an async

--- backend/src/app/api/bookings/route.ts
+++ backend/src/app/api/bookings/route.ts
@@ -6,23 +6,29 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
 
-export async function GET(
-  req: Request,
-  { params }: { params: Promise<{ id: string }> }
-) {
-  const { id } = await params
-
+export async function POST(req: Request) {
   try {
+    const body = await req.json()
+
+    const { user_id, hostel_id, check_in, check_out, guests } = body
+
     const { data, error } = await supabase
-      .from("hostels")
-      .select("*")
-      .eq("id", id)
-      .single()
+      .from("bookings")
+      .insert([
+        {
+          user_id,
+          hostel_id,
+          check_in,
+          check_out,
+          guests
+        }
+      ])
+      .select()
 
     if (error) {
       return NextResponse.json(
         { success: false, error: error.message },
-        { status: 404 }
+        { status: 500 }
       )
     }
 
---
+++ backend/src/app/api/bookings/user/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server"
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+)
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url)
+
+  const user_id = searchParams.get("user_id")
+
+  if (!user_id) {
+    return NextResponse.json(
+      { success: false, error: "user_id required" },
+      { status: 400 }
+    )
+  }
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*")
+    .eq("user_id", user_id)
+
+  if (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 }
+    )
+  }
+
+  return NextResponse.json({
+    success: true,
+    data
+  })
+}
